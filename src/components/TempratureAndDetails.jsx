72fe3d577fe556bf6db113ae75bdcc2fd2162b6f
Sumon Ahemed <ahemedsumon03@gmail.com>
2023-06-23 21:00:27 +0600

    Design TempratureAndDetails component

--- src/components/TempratureAndDetails.jsx
+++ src/components/TempratureAndDetails.jsx
@@ -4,7 +4,9 @@ import React from 'react'
 const TempratureAndDetails = () => {
     return (
         <div>
-            
+            <div className='flex items-center justify-center py-6 text-xl text-cyan-300'>
+                <p>Cloudy And Weather</p>
+            </div>
         </div>
     )
 }
