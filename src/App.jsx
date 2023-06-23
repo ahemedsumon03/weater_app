befe3fc7890fe6faad4136e1e0f27e77f93c77d5
Sumon Ahemed <ahemedsumon03@gmail.com>
2023-06-23 17:40:32 +0600

    Create react project and installed tailwind css in this project

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+/* eslint-disable no-unused-vars */
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='underline text-center'>
+      This is weater app
+    </div>
+  )
+}
+
+export default App
