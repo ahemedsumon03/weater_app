a36818141979c7651f75a28f2fe971bce558508e
Sumon Ahemed <ahemedsumon03@gmail.com>
2023-06-23 18:40:20 +0600

    Design Inputs and TimeAndLocation Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 /* eslint-disable no-unused-vars */
 import React from 'react'
+import Inputs from './components/Inputs'
+import TimeAndLocation from './components/TimeAndLocation'
+import TempratureAndDetails from './components/TempratureAndDetails'
 
 const App = () => {
   return (
-    <div className='underline text-center'>
-      This is weater app
+    <div className='mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br from-cyan-700 to-blue-700 h-fit shadow-xl'>
+      <Inputs />
+      <TimeAndLocation />
+      <TempratureAndDetails/>
     </div>
   )
 }
---
+++ src/components/TimeAndLocation.jsx
@@ -0,0 +1,20 @@
+/* eslint-disable no-unused-vars */
+import React from 'react'
+
+const TimeAndLocation = () => {
+    return (
+        <div>
+            <div className='flex items-center justify-center my-6'>
+                <p className='text-white text-xl font-extralight'>
+                    Friday, 23 June | Local Time : 6.26 PM
+                </p>
+            </div>
+
+            <div className='flex items-center justify-center my-3'>
+                <p className='text-white text-3xl font-medium'>Berlin,DE</p>
+            </div>
+        </div>
+    )
+}
+
+export default TimeAndLocation
